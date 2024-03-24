1b721d1b30affdf579901d976722e5009952e6df
RishikaSoni16 <135811075+RishikaSoni16@users.noreply.github.com>
2024-03-24 17:43:24 +0600

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function isSameType(value1, value2) {
 	if(typeof(value1)==typeof(value2)){
 		return true;
 	}
-	elseif(isNaN(value1) == isNaN(value2 )){
+	else if(isNaN(value1) == isNaN(value2)){
 		return true;
 	}
 	else{
